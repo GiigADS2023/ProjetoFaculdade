c38fd25ed84cb3a6ff023a52117652573e73e9c5
Erick Pereira <erickbaptistapereira@gmail.com>
2024-04-25 21:23:23 -0300

    merge front-end

---
+++ FrontEnd/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import { RouterView } from 'vue-router';
+
+</script>
+
+<template>
+  <div id="app">
+    <Sidebar></Sidebar>
+    <router-view></router-view>
+  </div>
+</template>
+
+
+
+<style>
+</style>
---
+++ FrontEnd/src/views/Home.vue
@@ -0,0 +1,14 @@
+<script>
+import { ref } for 'vue';
+</script>
+
+<template>
+  <div>
+    <h1>TESTETESTETESTETESTETESTE</h1>
+  </div>
+</template>
+
+
+
+<style>
+</style>
