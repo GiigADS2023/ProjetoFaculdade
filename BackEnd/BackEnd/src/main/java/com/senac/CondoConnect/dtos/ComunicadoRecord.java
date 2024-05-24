8907f9ab8c835d2a902c6b6c499ec57069506509
Matheus Avelino <avelinosantos09@gmail.com>
2024-05-24 19:29:58 -0300

    remove: removida pasta inutilizada dentro do projeto principal Java

--- BackEnd/BackEnd/src/main/java/com/senac/CondoConnect/dtos/ComunicadoRecord.java
+++
@@ -1,9 +0,0 @@
-package com.senac.CondoConnect.dtos;
-
-import java.time.LocalDate;
-
-import com.senac.CondoConnect.Model.UsuarioModel;
-
-public record ComunicadoRecord(String tituloComunicado, String descricaoComunicado, boolean tipoComunicado, LocalDate dataComunicado, UsuarioRecord usuario) {
-
-}
