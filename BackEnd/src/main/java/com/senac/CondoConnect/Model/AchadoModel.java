af90955d99ad07e2ef73f5345b63d3d6c257a4e1
DESKTOP-93J2E8M\schul <schultz.dauer@gmail.com>
2024-06-03 19:12:14 -0300

    Ajuste do Post Assembleia

--- BackEnd/src/main/java/com/senac/CondoConnect/Model/AchadoModel.java
+++ BackEnd/src/main/java/com/senac/CondoConnect/Model/AchadoModel.java
@@ -8,8 +8,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-@Entity
-@Table(name="Achado")
+    @Entity
+    @Table(name="Achado")
 public class AchadoModel {
 	
 	@Id
